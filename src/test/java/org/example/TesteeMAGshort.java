3ef5a78c8b7e3b29ec590b5b49f4435ddac2ef2a
Rrobertar <robertandrei.neagu@yahoo.com>
2025-03-06 11:43:22 +0200

    Update TesteeMAGshort.java

--- src/test/java/org/example/TesteeMAGshort.java
+++ src/test/java/org/example/TesteeMAGshort.java
@@ -10,45 +10,53 @@ public class TesteeMAGshort {
 public void setup () throws Exception{
 
 
-ContulmeuLogin test1= new ContulmeuLogin();
+ContulmeuLogin test1 = new ContulmeuLogin();
          test1.setup();
          test1.souldbeanswerWithTrue();
-         test.1.teardown();
+         test1.teardown();
 
-Baradecautare test2= new Baradecautare();
+Baradecautare test2 = new Baradecautare();
          test2.setup();
          test2.souldbeanswerWithTrue();
+         test2.teardown();
 
 AdaugainCos test3= new AdaugainCos();
          test3.setup();
          test3.souldbeanswerWithTrue();
+         test3.teardown();
 
-AdaugalaFavorite test4=  new AdaugalaFavorite();
+AdaugalaFavorite test4 = new AdaugalaFavorite();
          test4.setup();
          test4.souldbeanswerWithTrue();
+         test4.teardown();
 
 
-OfertaZilei test5= new OfertaZilei();
+OfertaZilei test5 = new OfertaZilei();
          test5.setup();
          test5.souldbeanswerWithTrue();
+         test5.teardown();
 
-Resigilate test6= new Resigilate();
+Resigilate test6 = new Resigilate();
         test6.setup();
         test6.souldbeanswerWithTrue();
+        test6.teardown();
 
-Logo test7= new Logo();
+Logo test7 = new Logo();
         test7.setup();
         test7.souldbeanswerWithTrue();
+        test7.teardown();
 
 
-ButonPLus test8= new ButonPLus();
+ButonPLus test8 = new ButonPLus();
         test8.setup();
         test8.souldbeanswerWithTrue();
+        test8.teardown();
 
 
 Sterge test9 = new Sterge();
         test9.setup();
         test9.souldbeanswerWithTrue();
+        test9.teardown();
 
     }
 }
